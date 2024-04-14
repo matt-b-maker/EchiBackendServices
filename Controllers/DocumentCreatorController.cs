d55b15ebefcf0503c9737ce69e734c24ee78f5c1
Sormin-the-White <wriffsdev@gmail.com>
2024-04-14 12:07:25 -0600

    added document creator back in

--- Controllers/DocumentCreatorController.cs
+++ Controllers/DocumentCreatorController.cs
@@ -1,6 +1,5 @@
 ﻿using EchiBackendServices.Models;
 using EchiBackendServices.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EchiBackendServices.Controllers
@@ -9,24 +8,24 @@ namespace EchiBackendServices.Controllers
     [ApiController]
     public class DocumentCreatorController(DocumentService documentService) : ControllerBase
     {
-        //[HttpPost]
-        //public async Task<IActionResult> CreateInspectionAgreement([FromBody] ClientModel clientModel)
-        //{
-        //    try
-        //    {
-        //        if (!clientModel.IncludeRadonAddendum)
-        //        {
-        //            var documentUrl = await documentService.CreateInspectionAgreement(clientModel);
-        //            return Ok(new[] { documentUrl });
-        //        }
-        //        var inspectionAgreementUrl = await documentService.CreateInspectionAgreement(clientModel);
-        //        var radonAddendumUrl = await documentService.CreateRadonAddendum(clientModel);
-        //        return Ok(new[] { inspectionAgreementUrl, radonAddendumUrl });
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
-        //    }
-        //}
+        [HttpPost]
+        public async Task<IActionResult> CreateInspectionAgreement([FromBody] ClientModel clientModel)
+        {
+            try
+            {
+                if (!clientModel.IncludeRadonAddendum)
+                {
+                    var documentUrl = await documentService.CreateInspectionAgreement(clientModel);
+                    return Ok(new[] { documentUrl });
+                }
+                var inspectionAgreementUrl = await documentService.CreateInspectionAgreement(clientModel);
+                var radonAddendumUrl = await documentService.CreateRadonAddendum(clientModel);
+                return Ok(new[] { inspectionAgreementUrl, radonAddendumUrl });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+            }
+        }
     }
 }
